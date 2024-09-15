187e44d1316d70551cee9b7ee84dd8f4eac2ad5b
Majho2141 <mariaj.munozp@autonoma.edu.co>
2024-09-15 16:13:06 -0500

    Boton editar y eliminar componentes

--- components/atoms/ButtonDelete.tsx
+++ components/atoms/ButtonDelete.tsx
@@ -1,10 +1,8 @@
 import { buttonTable } from "../tokens"
 
-type ButtonDeleteProps = {
-    text: string
-}
 
-export function ButtonDelete({text}: ButtonDeleteProps) {
+
+export function ButtonDelete() {
     return (
         <button className={`text-white bg-red-700 hover:bg-red-800 ${buttonTable}`}>Elimiar</button>
     )
